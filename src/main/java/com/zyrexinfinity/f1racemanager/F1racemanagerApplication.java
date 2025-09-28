e4d3a28f416f34f13111150893b5bc08a639e01d
ZyreX-Infinity <gluglevichk@gmail.com>
2025-09-28 14:48:44 +0300

    Initial Commit

---
+++ src/main/java/com/zyrexinfinity/f1racemanager/F1racemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.zyrexinfinity.f1racemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1racemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1racemanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zyrexinfinity/f1racemanager/F1racemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyrexinfinity.f1racemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1racemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
